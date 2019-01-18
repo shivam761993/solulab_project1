5677b12b6ddfd7617905337be7d805e12664a0b4
shivam761993 <46737934+shivam761993@users.noreply.github.com>
2019-01-18 17:52:12 +0530

    Add files via upload

--- final.js
+++ final.js
@@ -1,10 +1,14 @@
     // Writing the main logic for the stopwatch code in javascript.
     var ms = 0, s = 0, m = 0;
     var timer;
+    
+    
 
     // Storing the output values in newly defined variables.
     var stopwatch1 = document.querySelector('.stopwatch');
     var lapsContainer = document.querySelector('.laps');
+    var historyLogs = document.querySelector(".but5").addEventListener('click',saveToLocalStorage);
+                   
 
     function start() {
         if(!timer){
@@ -63,9 +67,42 @@
     }
 
     // Removing all the data and returning the start time.
-    function resetLaps() {
+    function resetLaps() { 
+        if(timer){
+        timer = false;
         lapsContainer.innerHTML = "";
         m = 0, ms = 0, s = 0;
         return (m) + ":" + (s) + ":" + (ms);
-        pause();
+        }
+     }
+     
+    function history(){
+        
     }
+    
+    // Here I am storing time logs in local storage.
+    function getFromLocalStorage() {
+        let history;
+        if(localStorage.getItem('historyls')==null) {
+            history = [];
+        } else {
+            history = JSON.parse(localStorage.getItem('historyls'));
+        }
+        return history;
+    }
+
+    function saveToLocalStorage(){
+        
+        let history = getFromLocalStorage();
+            {
+                if(history.length >=10){
+                    history.shift();
+                }
+            let timels = m + ':' + s + ':' + ms;
+            history.push(timels);
+            localStorage.setItem('historyls', JSON.stringify(history));
+            
+            }
+    }
+
+    
